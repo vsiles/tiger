1094b897df76f6d9bfdb060de2431859d1521b02
Vincent Siles <vincent.siles@ens-lyon.org>
2017-11-03 17:14:06 +0100

    improving symbol module + adding table for bindings
    Signed-off-by: Vincent Siles <vincent.siles@ens-lyon.org>

--- src/symbol.ml
+++ src/symbol.ml
@@ -1,4 +1,27 @@
-type t = string
+type t = int * string
 
-let mk name = name
-let name symbol = symbol
+let mk =
+    let table = Hashtbl.create 32 in
+    let n = ref (-1) in
+    function name ->
+        try
+            Hashtbl.find table name, name
+        with Not_found ->
+            incr n;
+            Hashtbl.add table name !n;
+            !n, name
+;;
+
+let name (_, s) = s
+;;
+
+module Ord = struct
+    (* to avoid confusion an write type t = t *)
+    type symbol = t
+    type t = symbol
+
+    let compare (n1, _) (n2, _) =
+        Pervasives.compare n1 n2
+end
+
+module Table = Map.Make (Ord)
--- src/symbol.mli
+++ src/symbol.mli
@@ -2,3 +2,5 @@ type t
 
 val mk: string -> t
 val name : t -> string
+
+module Table : Map.S with type key = t
